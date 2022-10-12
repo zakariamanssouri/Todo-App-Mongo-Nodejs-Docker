b6079c3b5e70880539cee4c7097a77c99a25c6f5
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-12 19:11:08 +0100

    update server js

--- public/server.js
+++ public/server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser=require("body-parser")
 const port = 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/static",express.static('public'))
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render('todo.ejs');
 });
+
+app.post("/", (req, res) => {
+ console.log(req.body)    // e
+});
+
 
 app.listen(port, () => {
   console.log(`app running in port ${port}`);
