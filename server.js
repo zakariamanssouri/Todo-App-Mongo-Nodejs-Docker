d669480f957d56d008add31928ee23b28d887f41
Zakaria <z.mansouri_etu@enset-media.ac.ma>
2022-10-12 18:30:48 +0100

    setup server to listenning on port 3000

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const port=3000;
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(port, () => {
+  console.log(`app running in port ${port}`);
+});
